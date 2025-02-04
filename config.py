8aabc9983b78790e16b012f7739976480a3db317
Nethzz <34731410+Nethzz@users.noreply.github.com>
2025-02-04 14:27:12 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+ANNOTATION_FILE = "path/to/instances_default.json" # paste your path to dataset
+IMAGE_FOLDER = "path/to/PORTIMAGES" # paste your path to images
+BATCH_SIZE = 4
+LEARNING_RATE = 0.001
+EPOCHS = 10
